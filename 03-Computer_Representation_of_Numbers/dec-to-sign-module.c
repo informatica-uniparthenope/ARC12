ad4cfe3082590e560fe44ce60ad6436f07f75ae6
Raffaele Montella <raffaele.montella@uniparthenope.it>
2021-10-10 13:21:15 +0200

    Added signed integer to binary as sign and module

---
+++ 03-Computer_Representation_of_Numbers/dec-to-sign-module.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv) {
+  if (argc==2) {
+    char dst[]="00000000";
+    int src=atoi(argv[1]);
+
+    if (src<0) {
+      dst[0] = '1';
+    }
+
+    src=abs(src);
+
+    int maxDigits=strlen(dst);
+    int idx=maxDigits-1;
+
+    while (src!=0 && idx>=0) {
+      dst[idx]='0'+src%2;
+      src=src/2;
+      idx--;
+    }
+    if (src==0) {  
+      printf("%s\n",dst);
+      return 0;
+    } else {
+      printf("ERROR: Overflow\n");
+    return -2;
+    }
+  } else {
+    printf("Convert an iteger to its sign and module binary representation.\n");
+    printf("Usage %s int\n",argv[0]);
+    return -1;
+  }
+}
